ffdf6faf4028563b41e428f7c04c125b68438bcc
Gyula Gombos <gombos88@gmail.com>
2022-10-14 20:31:28 +0200

    notes get method was created

--- Backend/src/main/java/com/gombino/mynotes/controller/NoteController.java
+++ Backend/src/main/java/com/gombino/mynotes/controller/NoteController.java
@@ -1,16 +1,18 @@
 package com.gombino.mynotes.controller;
 
-import com.gombino.mynotes.models.Note;
 import com.gombino.mynotes.repositories.NoteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/notes")
+@Slf4j
 public class NoteController {
     @Autowired
     NoteRepository noteRepository;
@@ -19,13 +21,19 @@ public class NoteController {
         this.noteRepository = noteRepository;
     }
 
-    @GetMapping
-    public ResponseEntity<Object> getNotes() {
-        Note note = new Note();
-        note.setText("Test note here!");
-        note.setIsUrgent(true);
-
-        return ResponseEntity.status(HttpStatus.OK).body(noteRepository.save(note));
+    @GetMapping()
+    public ResponseEntity<Object> getNotesByUrgentOrNotOrAll(@RequestParam(required = false) String isUrgent) {
+        if (isUrgent != null && !isUrgent.isEmpty()) {
+            Boolean isUrgentBoolean = false;
+            try {
+                isUrgentBoolean = Boolean.parseBoolean(isUrgent);
+            } catch (Exception exception) {
+                log.warn(exception.getMessage());
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(noteRepository.findAllUrgentNotes(isUrgentBoolean));
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(noteRepository.findAll());
+        }
     }
 
 }
--- Backend/src/main/java/com/gombino/mynotes/repositories/NoteRepository.java
+++ Backend/src/main/java/com/gombino/mynotes/repositories/NoteRepository.java
@@ -2,6 +2,18 @@ package com.gombino.mynotes.repositories;
 
 import com.gombino.mynotes.models.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface NoteRepository extends MongoRepository<Note, String> {
+
+
+    @Query("{name:'?0'}")
+    Note findItemByName(String name);
+
+    @Query(value = "{isUrgent: ?0 }", fields = "{'id' : 0, 'text' : 1, 'isUrgent' : 1}")
+    List<Note> findAllUrgentNotes(boolean isUrgent);
+
+    public long count();
 }
