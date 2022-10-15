e51c8b65074e9fc90323e43531f8d2717e4e23e9
Gyula Gombos <gombos88@gmail.com>
2022-10-15 22:52:54 +0200

    get notes querry was updated with more fields

--- Backend/src/main/java/com/gombino/mynotes/MyNotesApplication.java
+++ Backend/src/main/java/com/gombino/mynotes/MyNotesApplication.java
@@ -4,12 +4,26 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MyNotesApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("GET", "POST", "PUT", "DELETE");
+
+            }
+        };
     }
 
     public static void main(String[] args) {
--- Backend/src/main/java/com/gombino/mynotes/repositories/NoteRepository.java
+++ Backend/src/main/java/com/gombino/mynotes/repositories/NoteRepository.java
@@ -14,7 +14,7 @@ public interface NoteRepository extends MongoRepository<Note, String> {
     @Query("{name:'?0'}")
     Note findItemByName(String name);
 
-    @Query(value = "{isUrgent: ?0 }", fields = "{'id' : 0, 'text' : 1, 'isUrgent' : 1}")
+    @Query(value = "{isUrgent: ?0 }")
     List<Note> findAllUrgentNotes(boolean isUrgent);
 
     public long count();
