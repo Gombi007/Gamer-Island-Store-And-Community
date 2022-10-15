aa31d735c881c8adc2cbc43abc6311a0b6d0c5b1
Gyula Gombos <gombos88@gmail.com>
2022-10-15 22:52:08 +0200

    first api get connection was tested

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConfigService } from './config/config.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My-notes';
+  notes: noteDto[] = [];
+
+
+  constructor(private service: ConfigService) { }
+
+  showNotes() {
+    this.service.getNotes('').subscribe((data: noteDto[]) => {
+      this.notes = data;
+    });
+  }
+
+}
+
+export class noteDto {
+  id: string = "";
+  text: string = "";
+  text2: string = "";
+  isUrgent: boolean = false;
+  created: string = "";
+  lastModified: string = "";
+  imgUrl: string = "";
 }
