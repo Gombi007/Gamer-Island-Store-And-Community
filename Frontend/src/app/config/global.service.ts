bdeb4fcf8aff4190919fd06eae29b39fb0a5f5bd
Gyula Gombos <gombos88@gmail.com>
2022-10-19 13:37:41 +0200

    navigation style was updated

--- Frontend/src/app/config/global.service.ts
+++ Frontend/src/app/config/global.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from "@angular/core";
-import { noteDto } from "../notes/config/note.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class NoteService {
     constructor() { }
-    noteToModify = null;
 
 }
