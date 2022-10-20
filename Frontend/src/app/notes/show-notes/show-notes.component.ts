75aeced4680e69f3cb17975522a6dbfa27742b7c
Gyula Gombos <gombos88@gmail.com>
2022-10-20 18:11:09 +0200

    isPendin was added to all method

--- Frontend/src/app/notes/show-notes/show-notes.component.ts
+++ Frontend/src/app/notes/show-notes/show-notes.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { tap } from 'rxjs';
 import { noteDto } from '../config/note.model';
 import { NoteService } from '../config/note.service';
 
@@ -11,6 +12,7 @@ import { NoteService } from '../config/note.service';
 export class ShowNotesComponent implements OnInit {
   currentlyRouteAfterUrgentTag = '';
   notes: noteDto[] = [];
+  isPending: boolean = false;
 
   constructor(private noteService: NoteService, private route: ActivatedRoute, private router: Router) { }
 
@@ -26,44 +28,70 @@ export class ShowNotesComponent implements OnInit {
 
   showNotes(isUrgent: string = '') {
     if (isUrgent === '' || isUrgent === 'all') {
-      this.noteService.getNotes().subscribe({
-        next: (data) => this.notes = data
-      });
+      this.noteService.getNotes()
+        .pipe(tap(() => { this.isPending = true }))
+        .subscribe({
+          next: (data) => {
+            this.notes = data
+            this.isPending = false
+          }
+        });
     }
 
     if (isUrgent === 'urgent') {
-      this.noteService.getNotesWithIsUrgentParam('true').subscribe({
-        next: (data) => this.notes = data
-      });
+      this.noteService.getNotesWithIsUrgentParam('true')
+        .pipe(tap(() => { this.isPending = true }))
+        .subscribe({
+          next: (data) => {
+            this.notes = data
+            this.isPending = false
+          }
+        });
     }
     if (isUrgent === 'non-urgent') {
-      this.noteService.getNotesWithIsUrgentParam('false').subscribe({
-        next: (data) => this.notes = data
-      });
+      this.noteService.getNotesWithIsUrgentParam('false')
+        .pipe(tap(() => { this.isPending = true }))
+        .subscribe({
+          next: (data) => {
+            this.notes = data
+            this.isPending = false
+          }
+        });
     }
   }
 
   removeNote(noteId: string) {
-    this.noteService.removeNote(noteId).subscribe({
-      next: () => {
-        this.showNotes(this.currentlyRouteAfterUrgentTag);
-      }
-    });
+    if (!this.isPending) {
+      this.noteService.removeNote(noteId)
+        .pipe(tap(() => { this.isPending = true }))
+        .subscribe({
+          next: () => {
+            this.showNotes(this.currentlyRouteAfterUrgentTag);
+            this.isPending = false;
+          }
+        });
+    }
   }
 
   editNote(note: noteDto) {
-    this.noteService.noteToModify = note;
-    this.router.navigate(['/notes/add/create-note'])
+    if (!this.isPending) {
+      this.noteService.noteToModify = note;
+      this.router.navigate(['/notes/add/create-note'])
+    }
   }
 
   changeUrgentOrNotNoteStatus(note: noteDto, noteId: string) {
-    note.isUrgent = !note.isUrgent;
-    this.noteService.modifyNote(note, noteId).subscribe({
-      next: () => {
-        this.showNotes(this.currentlyRouteAfterUrgentTag);
-      }
-    });
-
+    if (!this.isPending) {
+      note.isUrgent = !note.isUrgent;
+      this.noteService.modifyNote(note, noteId)
+        .pipe(tap(() => { this.isPending = true }))
+        .subscribe({
+          next: () => {
+            this.showNotes(this.currentlyRouteAfterUrgentTag);
+            this.isPending = false;
+          }
+        });
+    }
   }
 
 }
