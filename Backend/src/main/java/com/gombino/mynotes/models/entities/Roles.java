8c7afdcdeb46e4243c16b372f2005c1cfa64b19b
Gyula Gombos <gombos88@gmail.com>
2022-10-21 12:40:58 +0200

    user and rolw entities were created

--- Backend/src/main/java/com/gombino/mynotes/models/entities/Roles.java
+++ Backend/src/main/java/com/gombino/mynotes/models/entities/Roles.java
@@ -1,2 +1,19 @@
-package com.gombino.mynotes.models.entities;public class Roles {
+package com.gombino.mynotes.models.entities;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Tag(name = "Role entity")
+public class Roles {
+    @Id
+    private String id;
+    private String roleName;
 }
--- Backend/src/main/java/com/gombino/mynotes/models/entities/User.java
+++ Backend/src/main/java/com/gombino/mynotes/models/entities/User.java
@@ -1,2 +1,26 @@
-package com.gombino.mynotes.models.entities;public class User {
+package com.gombino.mynotes.models.entities;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document("users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Tag(name = "User entity")
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String avatar;
+    private Instant lasLogin;
+    private Boolean isDisabled;
 }
