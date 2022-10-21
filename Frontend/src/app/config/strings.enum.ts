2839c0d4a5a08b1a22591cbbebe735160fdb0f1a
Gyula Gombos <gombos88@gmail.com>
2022-10-22 00:40:09 +0200

    url list was updated

--- Frontend/src/app/config/strings.enum.ts
+++ Frontend/src/app/config/strings.enum.ts
@@ -2,9 +2,9 @@ export enum STRINGS {
     SERVER_URL = "http://localhost:8081/",
     API_LOGIN = "api/login",
     API_REGISTRATION = "api/registration",
-    API_GET_NOTES = "api/notes",
+    API_GET_NOTES = "api/notes/",
     API_GET_NOTES_WITH_ISURGENT = "api/notes?isUrgent=",
-    API_POST_NOTE = "api/notes",
+    API_POST_NOTE = "api/notes/",
     API_PUT_NOTE = "api/notes?id=",
     API_REMOVE_NOTE = "api/notes?id=",
 }
