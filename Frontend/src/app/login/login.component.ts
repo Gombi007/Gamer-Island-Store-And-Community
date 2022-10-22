400420275830947729ae2b9e5fa696ea9f51f759
Gyula Gombos <gombos88@gmail.com>
2022-10-22 15:29:59 +0200

    pendig was added to ligon and registration

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { timer } from 'rxjs';
 import { AuthenticateService } from '../config/authenticate.service';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -16,6 +17,7 @@ export class LoginComponent implements OnInit {
   errorResponse = '';
   registrationOk = '';
   expiredSession: null | string = null;
+  isPending = false;
 
   constructor(private authService: AuthenticateService, private router: Router, private route: ActivatedRoute) {
     localStorage.clear();
@@ -47,18 +49,22 @@ export class LoginComponent implements OnInit {
     this.showLoginForm = !this.showLoginForm;
   }
 
-  stratLogin() {
-    if (this.loginForm.valid) {
 
+  startLogin() {
+
+    if (this.loginForm.valid) {
+      this.isPending = true;
       this.authService.loginViaBackend(this.loginForm.value).subscribe({
         next: (data: any) => {
           localStorage.setItem('user_id', data.user_id);
           localStorage.setItem('token', data.token);
           this.loginForm.reset();
+          this.isPending = false;
           this.router.navigate(['']);
         },
         error: (response) => {
           this.errorResponse = response.error;
+          this.isPending = false;
           timer(3000).subscribe(
             () => this.errorResponse = ''
           );
@@ -66,19 +72,23 @@ export class LoginComponent implements OnInit {
       });
     }
   }
+
   startRegistration() {
     if (this.registrationForm.valid) {
+      this.isPending = true;
       this.authService.registerViaBackend(this.registrationForm.value).subscribe({
         next: () => {
           this.showLoginForm = true;
           this.registrationOk = 'Registration OK, Please Login'
           this.registrationForm.reset();
+          this.isPending = false;
           timer(3000).subscribe(
             () => this.registrationOk = ''
           );
         },
         error: (response) => {
           this.errorResponse = response.error;
+          this.isPending = false;
           timer(3000).subscribe(
             () => this.errorResponse = ''
           );
