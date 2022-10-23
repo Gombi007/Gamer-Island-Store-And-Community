a34f454c253bc25950f97b163ffb9aba25acdc23
Gyula Gombos <gombos88@gmail.com>
2022-10-23 21:50:36 +0200

    edit profile design was updated

--- Frontend/src/app/profile/edit-profile/edit-profile.component.ts
+++ Frontend/src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
+  profileForm: FormGroup
+  passChangeForm: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.createProfileForm();
+    this.createpassChangeForm();
+  }
+
+  createProfileForm() {
+    return this.profileForm = new FormGroup({
+      'username': new FormControl({ value: "Username", disabled: true }),
+      'password': new FormControl(null, [Validators.required]),
+      'email': new FormControl(null,),
+      'avatar': new FormControl(null,),
+    });
+  }
+
+  createpassChangeForm() {
+    return this.passChangeForm = new FormGroup({
+      'oldPassword': new FormControl(null, [Validators.required]),
+      'newPassword': new FormControl(null, [Validators.required]),
+      'reNewPassword': new FormControl(null, [Validators.required]),
+    });
+  }
+
+
+
+  saveProfile() {
+
   }
 
 }
