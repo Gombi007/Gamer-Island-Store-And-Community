ec53c6a7c31f40ece5830c175122ec9d0f23dde8
Gyula Gombos <gombos88@gmail.com>
2022-10-23 20:42:29 +0200

    menu was added to profile

--- Frontend/src/app/angular-material.module.ts
+++ Frontend/src/app/angular-material.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 const MaterialComponents = [
@@ -31,7 +32,8 @@ const MaterialComponents = [
     MatInputModule,
     MatRadioModule,
     MatCheckboxModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatMenuModule
 ];
 
 @NgModule({
