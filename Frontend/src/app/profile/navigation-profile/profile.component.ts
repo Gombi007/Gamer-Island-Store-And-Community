f4ce1e874a62d0b39290e004065e4581910c8984
Gyula Gombos <gombos88@gmail.com>
2022-10-24 17:44:53 +0200

    profile was updated with avatar and username in the navbar

--- Frontend/src/app/profile/navigation-profile/profile.component.ts
+++ Frontend/src/app/profile/navigation-profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../config/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  username = '';
+  avatar = 'Username';
+  isPending = false;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.getProfileData();
+  }
+
+  getProfileData() {
+    this.isPending = true;
+    this.profileService.getProfileData().subscribe({
+      next: (data) => {
+        this.username = data.username;
+        this.avatar = data.avatar;
+        this.isPending = false
+      },
+      error: () => {
+        this.isPending = false
+      }
+    });
+
   }
 
 }
