830f9f30c82dbe166e3fbebf324ea00c9b69cee6
Gyula Gombos <gombos88@gmail.com>
2022-11-08 21:12:37 +0100

    frontend websocket with stomp and sockj was added

--- Frontend/src/app/config/strings.enum.ts
+++ Frontend/src/app/config/strings.enum.ts
@@ -7,5 +7,6 @@ export enum STRINGS {
     API_NOTES_CHANGE_VISIBILITY = "api/notes/change-visibility/",
     API_PROFILE = "api/profile/",
     API_PROFILE_CHANGE_PASSWORD = "api/profile/change-password/",
+    WEB_SOCKET = "ws-server",
 
 }
