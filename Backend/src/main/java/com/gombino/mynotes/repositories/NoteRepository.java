6e6472f574d681ad9b19ac135e56dc830023b962
Gyula Gombos <gombos88@gmail.com>
2022-11-09 17:25:04 +0100

    new native queries were added

--- Backend/src/main/java/com/gombino/mynotes/repositories/NoteRepository.java
+++ Backend/src/main/java/com/gombino/mynotes/repositories/NoteRepository.java
@@ -1,6 +1,8 @@
 package com.gombino.mynotes.repositories;
 
 import com.gombino.mynotes.models.entities.Note;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,15 +12,19 @@ import java.util.List;
 @Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
 
+    Page<Note> findByVisibilityOnlyForMe(boolean privateNote, Pageable pageable);
+
+    @Query("{$and:[{ id:{$in:?0}} , {$or: [{visibilityOnlyForMe:false} , {creatorId:?1} ]} ]}")
+    Page<Note> findFavoriteNotes(List<String> ids, String creatorId, Pageable pageable);
+
+    @Query(value = "{'creatorId':?0}")
+    Page<Note> findAllAllNotesByUser(String creatorId, Pageable pageable);
 
     @Query("{name:'?0'}")
     Note findItemByName(String name);
 
     @Query(value = "{'visibilityOnlyForMe':false }")
     List<Note> findAllPublicNotes();
-
-    @Query(value = "{'creatorId':?0}")
-    List<Note> findAllAllNotesByUser(String creatorId);
 
     public long count();
 }
