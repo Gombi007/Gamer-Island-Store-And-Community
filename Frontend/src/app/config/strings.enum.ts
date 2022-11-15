af430c7ceb13788ca5bf13f499b944739bb1266e
Gyula Gombos <gombos88@gmail.com>
2022-11-15 21:34:20 +0100

    jwt token expiration date was modified

--- Frontend/src/app/config/strings.enum.ts
+++ Frontend/src/app/config/strings.enum.ts
@@ -1,5 +1,5 @@
 export enum STRINGS {
-    SERVER_URL = "http://localhost:8443/",
+    SERVER_URL = "http://localhost:8081/",
     API_LOGIN = "api/login",
     API_REGISTRATION = "api/registration",
     API_NOTES = "api/notes/",
