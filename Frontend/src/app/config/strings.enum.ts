5b15583c0ca7189c6d0f7ee485cc73441bd617e2
Gyula Gombos <gombos88@gmail.com>
2022-11-21 19:29:20 +0100

    websocket serve url was added

--- Frontend/src/app/config/strings.enum.ts
+++ Frontend/src/app/config/strings.enum.ts
@@ -1,5 +1,6 @@
 export enum STRINGS {
     SERVER_URL = "http://localhost:8081/",
+    WEB_SOCKET = "ws://localhost:8081/ws-server",
     API_LOGIN = "api/login",
     API_REGISTRATION = "api/registration",
     API_NOTES = "api/notes/",
@@ -7,6 +8,5 @@ export enum STRINGS {
     API_NOTES_CHANGE_VISIBILITY = "api/notes/change-visibility/",
     API_PROFILE = "api/profile/",
     API_PROFILE_CHANGE_PASSWORD = "api/profile/change-password/",
-    WEB_SOCKET = "ws-server",
 
 }
