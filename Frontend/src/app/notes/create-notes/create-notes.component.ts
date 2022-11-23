f5f60c7e968d76df456536adc8e63804746de175
Gyula Gombos <gombos88@gmail.com>
2022-11-24 00:15:15 +0100

    refactoring for the simplify due to the new warn dialog componenet

--- Frontend/src/app/notes/create-notes/create-notes.component.ts
+++ Frontend/src/app/notes/create-notes/create-notes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Observable, timer } from 'rxjs';
@@ -13,7 +13,7 @@ import { NoteService } from '../config/note.service';
   templateUrl: './create-notes.component.html',
   styleUrls: ['./create-notes.component.scss']
 })
-export class CreateNotesComponent implements OnInit {
+export class CreateNotesComponent implements OnInit, OnDestroy {
 
   @ViewChild('form') form: FormGroupDirective;
 
@@ -39,13 +39,13 @@ export class CreateNotesComponent implements OnInit {
 
   createTheForm() {
     return this.createNoteForm = new FormGroup({
-      'title': new FormControl(null, [Validators.required, Validators.maxLength(30)]),
-      'text': new FormControl(null, [Validators.required, Validators.maxLength(160)]),
-      'link': new FormControl(null),
+      'title': new FormControl('', [Validators.required, Validators.maxLength(40)]),
+      'text': new FormControl('', [Validators.required, Validators.maxLength(160)]),
+      'link': new FormControl(''),
       'imgUrl': new FormControl(''),
       'ytUrl': new FormControl(''),
-      'videoUrl': new FormControl(''),
       'videoPosterUrl': new FormControl(''),
+      'videoUrl': new FormControl(''),
       'isFavorite': new FormControl(false),
       'visibilityOnlyForMe': new FormControl(true),
     });
@@ -58,23 +58,10 @@ export class CreateNotesComponent implements OnInit {
       'link': modifyNote.link,
       'imgUrl': modifyNote.imgUrl,
       'ytUrl': modifyNote.ytUrl,
-      'videoUrl': modifyNote.videoUrl,
       'videoPosterUrl': modifyNote.videoPosterUrl,
+      'videoUrl': modifyNote.videoUrl,
       'isFavorite': modifyNote.isFavorite,
       'visibilityOnlyForMe': modifyNote.visibilityOnlyForMe
-    });
-  }
-
-
-  cancelModifyAndBack() {
-    let warningText1 = 'You will lost the all unsaved data!';
-    let warningText2 = 'Are you sure to go back?';
-
-    this.openWarnDialog(warningText1, warningText2).subscribe((userConfirm) => {
-      if (userConfirm) {
-        this.noteService.noteToModify = undefined;
-        this.router.navigate([this.noteService.cancelModifyOrSubmitAndGoBack]);
-      }
     });
   }
 
@@ -120,7 +107,64 @@ export class CreateNotesComponent implements OnInit {
     }
   }
 
-  openWarnDialog(warningText1: string, warningText2: string): Observable<any> {
+  cancelModifyAndBack() {
+    let isEmptyFrom = true;
+    let warningText1 = 'You will lost the all unsaved data!';
+    let warningText2 = 'Are you sure to go back?';
+
+    Object.keys(this.createNoteForm.controls).forEach(key => {
+      let controlValue = this.createNoteForm.controls[key].value;
+      if (typeof controlValue !== "boolean" && controlValue !== null && controlValue !== '') {
+        isEmptyFrom = false;
+      }
+    });
+
+    if (isEmptyFrom) {
+      this.noteService.noteToModify = undefined;
+      this.router.navigate([this.noteService.cancelModifyOrSubmitAndGoBack]);
+    }
+
+    if (!isEmptyFrom) {
+      this.openWarnDialog(warningText1, warningText2).subscribe((userConfirm) => {
+        if (userConfirm) {
+          this.noteService.noteToModify = undefined;
+          this.router.navigate([this.noteService.cancelModifyOrSubmitAndGoBack]);
+        }
+      });
+    }
+  }
+
+
+
+  addImageOrVideoLink(label: string) {
+    let warningText1 = 'If you switch other media tab,<br>You will lost the current media data.';
+    let warningText2 = 'Are you sure to swicth media tab?';
+    let isEmptyAllMediaUrlField = true;
+
+    Object.keys(this.createNoteForm.controls).forEach((key: string) => {
+      let controlValue: string = this.createNoteForm.controls[key].value;
+      if (key.toLocaleLowerCase().includes('url') && controlValue !== null && controlValue.length > 0) {
+        isEmptyAllMediaUrlField = false;
+      }
+    });
+
+    if (isEmptyAllMediaUrlField) {
+      this.changeMediaTypeInForm(label);
+    }
+
+    if (!isEmptyAllMediaUrlField) {
+      this.openWarnDialog(warningText1, warningText2).subscribe((userConfirm: boolean) => {
+        if (userConfirm) {
+          Object.keys(this.createNoteForm.controls).forEach((key: string) => {
+            key.toLocaleLowerCase().includes('url') ? this.createNoteForm.controls[key].setValue('') : '';
+          });
+          this.changeMediaTypeInForm(label);
+        }
+      });
+    }
+  }
+
+  private openWarnDialog(warningText1: string, warningText2: string): Observable<any> {
     let dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.panelClass = '';
@@ -134,53 +178,25 @@ export class CreateNotesComponent implements OnInit {
     return dialog.afterClosed();
   }
 
-  addImageOrVideoLink(label: string) {
-    let imgUrlField = this.createNoteForm.controls['imgUrl'];
-    let ytUrlField = this.createNoteForm.controls['ytUrl'];
-    let videoUrlField = this.createNoteForm.controls['videoUrl'];
-    let videoPosterUrlField = this.createNoteForm.controls['videoPosterUrl'];
-    let warningText1 = 'If you switch other media tab,<br>You will lost the unsaved media data.';
-    let warningText2 = 'Are you sure to swicth media tab?';
-
-    //Only one option is selectable from image/youtube/other-video and all other will be removed 
-    // Reminding the user about this info
-    if (imgUrlField.value !== '' || ytUrlField.value !== '' || videoUrlField.value !== '' || videoPosterUrlField.value !== '') {
-      this.openWarnDialog(warningText1, warningText2).subscribe((userConfirm: boolean) => {
-        if (userConfirm) {
-          imgUrlField.setValue('');
-          ytUrlField.setValue('');
-          videoUrlField.setValue('');
-          videoPosterUrlField.setValue('');
-        }
-      });
-
+  private changeMediaTypeInForm(label: string) {
+    if (label === this.clickedUrlBtn) {
+      this.clickedUrlBtn = "";
     } else {
-      if (label === this.clickedUrlBtn) {
-        this.clickedUrlBtn = "";
-      } else {
-        this.clickedUrlBtn = label;
-      }
+      this.clickedUrlBtn = label;
     }
   }
 
   private checkWhatUrlIsPresent() {
     let givenUrlFieldName = '';
-    let imgUrlFieldValue = this.createNoteForm.controls['imgUrl'].value;
-    let ytUrlFieldValue = this.createNoteForm.controls['ytUrl'].value;
-    let videoUrlFieldValue = this.createNoteForm.controls['videoUrl'].value;
-    let videoPosterUrlFieldValue = this.createNoteForm.controls['videoPosterUrl'].value;
-
-    if (imgUrlFieldValue !== null && imgUrlFieldValue !== '') {
-      givenUrlFieldName = 'imgUrl';
-    }
-
-    if (ytUrlFieldValue !== null && ytUrlFieldValue !== '') {
-      givenUrlFieldName = 'ytUrl';
-    }
-
-    if ((videoUrlFieldValue !== null && videoUrlFieldValue !== '') || (videoPosterUrlFieldValue !== null && videoPosterUrlFieldValue !== '')) {
-      givenUrlFieldName = 'videoUrl';
-    }
+    Object.keys(this.createNoteForm.controls).forEach((key: string) => {
+      let controlValue: string = this.createNoteForm.controls[key].value;
+      if (key.toLocaleLowerCase().includes('url') && controlValue !== null && controlValue.length > 0)
+        givenUrlFieldName = key;
+    });
     return givenUrlFieldName;
+  }
+
+  ngOnDestroy(): void {
+    this.noteService.noteToModify = undefined;
   }
 }
