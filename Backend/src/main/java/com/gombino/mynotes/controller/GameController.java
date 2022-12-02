01a388cff8bd928962788864f039631b1743d7cb
Gyula Gombos <gombos88@gmail.com>
2022-12-02 14:51:10 +0100

    game controller was created

---
+++ Backend/src/main/java/com/gombino/mynotes/controller/GameController.java
@@ -0,0 +1,31 @@
+package com.gombino.mynotes.controller;
+
+import com.gombino.mynotes.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/games")
+@Slf4j
+@RequiredArgsConstructor
+@Tag(name = "GameController", description = "Manages games")
+public class GameController {
+    private final GameService gameService;
+
+    @Operation(description = "List all games")
+    @GetMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Object> getAllGame() {
+        return ResponseEntity.status(HttpStatus.OK).body(gameService.findAllGame());
+    }
+
+
+}
