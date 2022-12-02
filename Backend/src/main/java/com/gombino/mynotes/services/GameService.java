a2124d3622c3ff1b01654f533e7701580d548d25
Gyula Gombos <gombos88@gmail.com>
2022-12-02 14:50:23 +0100

    basic game service was created

---
+++ Backend/src/main/java/com/gombino/mynotes/services/GameService.java
@@ -0,0 +1,8 @@
+package com.gombino.mynotes.services;
+
+import java.util.Map;
+
+public interface GameService {
+
+    Map<String, Object> findAllGame();
+}
---
+++ Backend/src/main/java/com/gombino/mynotes/services/GameServiceImpl.java
@@ -0,0 +1,37 @@
+package com.gombino.mynotes.services;
+
+import com.gombino.mynotes.models.entities.Game;
+import com.gombino.mynotes.repositories.GameRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class GameServiceImpl implements GameService {
+    private final GameRepository gameRepository;
+
+    @Override
+    public Map<String, Object> findAllGame() {
+        Pageable paging = PageRequest.of(0, 5);
+        Page<Game> gamePage = gameRepository.findAll(paging);
+        List<Game> gameList = new ArrayList<>();
+        for (Game game : gamePage) {
+            gameList.add(game);
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", gameList);
+        return map;
+    }
+}
