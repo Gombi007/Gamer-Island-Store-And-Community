9c0fb117be1fa86771146cb08818435f61f7bf07
Gyula Gombos <gombos88@gmail.com>
2022-12-05 22:42:19 +0100

    steam api service interface was updated

--- Backend/src/main/java/com/gombino/mynotes/services/SteamApiService.java
+++ Backend/src/main/java/com/gombino/mynotes/services/SteamApiService.java
@@ -1,7 +1,11 @@
 package com.gombino.mynotes.services;
 
+import java.util.List;
+
 public interface SteamApiService {
 
     void saveAllSteamProductsInTheDB();
+
+    List<String> getSteamGames(Integer page, Integer size);
 
 }
