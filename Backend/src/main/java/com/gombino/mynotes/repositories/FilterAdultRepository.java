1b38cd0a6288e1dca6ab67cc2a293bc44c2de514
Gyula Gombos <gombos88@gmail.com>
2022-12-05 22:41:23 +0100

    adult filter was updated with get words query

--- Backend/src/main/java/com/gombino/mynotes/repositories/FilterAdultRepository.java
+++ Backend/src/main/java/com/gombino/mynotes/repositories/FilterAdultRepository.java
@@ -2,6 +2,7 @@ package com.gombino.mynotes.repositories;
 
 import com.gombino.mynotes.models.entities.FilterAdult;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,4 +10,9 @@ import java.util.ArrayList;
 @Repository
 public interface FilterAdultRepository extends MongoRepository<FilterAdult, String> {
     ArrayList<FilterAdult> findAll();
+
+    @Query(value = "{}", fields = "{'_id':0,'word':1}")
+    ArrayList<FilterAdult> getAllWords();
+
+
 }
