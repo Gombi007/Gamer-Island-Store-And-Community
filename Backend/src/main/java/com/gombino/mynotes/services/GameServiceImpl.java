65cdd23ff0232fd438e84dbab4b059317259890b
Gyula Gombos <gombos88@gmail.com>
2022-12-06 11:23:15 +0100

    game dto converter was added

--- Backend/src/main/java/com/gombino/mynotes/services/GameServiceImpl.java
+++ Backend/src/main/java/com/gombino/mynotes/services/GameServiceImpl.java
@@ -1,11 +1,13 @@
 package com.gombino.mynotes.services;
 
+import com.gombino.mynotes.models.dto.GameDto;
 import com.gombino.mynotes.models.dto.PaginationInfo;
 import com.gombino.mynotes.models.dto.PaginationSorterDto;
 import com.gombino.mynotes.models.entities.Game;
 import com.gombino.mynotes.repositories.GameRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -24,19 +26,24 @@ import java.util.Map;
 @Transactional
 public class GameServiceImpl implements GameService {
     private final GameRepository gameRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public Map<String, Object> findAllGame(PaginationSorterDto paginationSorterDto) {
         Pageable paging = PageRequest.of(paginationSorterDto.getPage(), paginationSorterDto.getSize(), Sort.by(paginationSorterDto.getSortBy()).ascending());
         Page<Game> gamePage = gameRepository.findAll(paging);
-        List<Game> gameList = new ArrayList<>();
+        List<GameDto> gameList = new ArrayList<>();
         for (Game game : gamePage) {
-            gameList.add(game);
+            gameList.add(convertToGameDto(game));
         }
         PaginationInfo paginationInfo = new PaginationInfo(gamePage.getNumber(), gamePage.getTotalPages(), gamePage.getTotalElements());
         Map<String, Object> map = new HashMap<>();
         map.put("page", gameList);
         map.put("paginationInfo", paginationInfo);
         return map;
+    }
+
+    private GameDto convertToGameDto(Game game) {
+        return modelMapper.map(game, GameDto.class);
     }
 }
