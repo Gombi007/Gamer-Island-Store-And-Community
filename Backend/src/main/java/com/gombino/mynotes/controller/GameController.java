0461c899e57f3ffbaf305a1434fbd17ae178e067
Gyula Gombos <gombos88@gmail.com>
2022-12-06 11:11:36 +0100

    game controller was updated with pagination info

--- Backend/src/main/java/com/gombino/mynotes/controller/GameController.java
+++ Backend/src/main/java/com/gombino/mynotes/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.gombino.mynotes.controller;
 
+import com.gombino.mynotes.models.dto.PaginationSorterDto;
 import com.gombino.mynotes.services.GameService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,8 +25,12 @@ public class GameController {
     @Operation(description = "List all games")
     @GetMapping
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<Object> getAllGame() {
-        return ResponseEntity.status(HttpStatus.OK).body(gameService.findAllGame());
+    public ResponseEntity<Object> getAllGame(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "1") int size,
+            @RequestParam(name = "sortBy", defaultValue = "") String sortBy) {
+        PaginationSorterDto paginationSorterDto = new PaginationSorterDto(page, size, sortBy);
+        return ResponseEntity.status(HttpStatus.OK).body(gameService.findAllGame(paginationSorterDto));
     }
 
 
