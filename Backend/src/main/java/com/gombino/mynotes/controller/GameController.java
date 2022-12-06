281c89dadee8d2cfc3d593169e955594a04beef4
Gyula Gombos <gombos88@gmail.com>
2022-12-06 11:36:45 +0100

    get game by id method was added

--- Backend/src/main/java/com/gombino/mynotes/controller/GameController.java
+++ Backend/src/main/java/com/gombino/mynotes/controller/GameController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/games")
@@ -31,6 +28,13 @@ public class GameController {
             @RequestParam(name = "sortBy", defaultValue = "") String sortBy) {
         PaginationSorterDto paginationSorterDto = new PaginationSorterDto(page, size, sortBy);
         return ResponseEntity.status(HttpStatus.OK).body(gameService.findAllGame(paginationSorterDto));
+    }
+
+    @Operation(description = "Get game by ID")
+    @GetMapping("/{gameId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Object> getGameById(@PathVariable String gameId) {
+        return ResponseEntity.status(HttpStatus.OK).body(gameService.findGameById(gameId));
     }
 
 
