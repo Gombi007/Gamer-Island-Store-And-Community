d423be5d11eca48be29bcf878b69669a5bb33ea4
Gyula Gombos <gombos88@gmail.com>
2022-12-08 18:19:51 +0100

    unnecessary queries were removed, due to use criteria instead

--- Backend/src/main/java/com/gombino/mynotes/repositories/GameRepository.java
+++ Backend/src/main/java/com/gombino/mynotes/repositories/GameRepository.java
@@ -4,7 +4,6 @@ import com.gombino.mynotes.models.entities.Game;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,18 +11,4 @@ public interface GameRepository extends MongoRepository<Game, String> {
 
     Page<Game> findAll(Pageable pageable);
 
-    @Query("{'isFree' : false}")
-    Page<Game> findAllWithoutFree(Pageable pageable);
-
-    @Query("{'price' : { $lt: ?0 } }")
-    Page<Game> findAllByPriceLessThan(Integer price, Pageable pageable);
-
-    @Query("{'isFree' : true}")
-    Page<Game> findAllisFree(Pageable pageable);
-
-    @Query("{$or: [{'isFree' : true},{'price' : { $lt: ?0 }}]}")
-    Page<Game> findAllByPriceOrFree(Integer price, Pageable pageable);
-
-    @Query("{$and: [{'isFree' : false},{'price' : { $lt: ?0 }}]}")
-    Page<Game> findAllByPriceWithoutFree(Integer price, Pageable pageable);
 }
