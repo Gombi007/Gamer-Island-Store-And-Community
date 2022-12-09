e9b585441f6f4f76bed20ddb95b535e64cc10f7e
Gyula Gombos <gombos88@gmail.com>
2022-12-09 12:56:48 +0100

    game search dto was updated

--- Backend/src/main/java/com/gombino/mynotes/models/dto/GameSearchDto.java
+++ Backend/src/main/java/com/gombino/mynotes/models/dto/GameSearchDto.java
@@ -10,16 +10,16 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameSearchDto {
-    private Integer price;
+    private String sortByField;
+    private Boolean isAscending;
+    private List<String> languages;
+    private List<String> genres;
+    private List<String> opSystems;
+    private List<String> categories;
     private Boolean isHideFreeGames;
     private Boolean isHideAdultGames;
     private Boolean showOnlyAdultGames;
+    private Integer price;
     private Boolean isHideMyOwnGames;
     private Boolean isHideMyWishlistGames;
-    private Boolean isAscending;
-    private String sortByField;
-    private List<String> genres;
-    private List<String> opSystems;
-    private List<String> languages;
-    private List<String> categories;
 }
