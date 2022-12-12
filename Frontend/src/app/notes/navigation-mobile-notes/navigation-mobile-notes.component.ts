477408ff9bfeba33eeb86f4c9769e064dec84700
Gyula Gombos <gombos88@gmail.com>
2022-12-12 20:30:59 +0100

    fixed number value was replaced to const enum

--- Frontend/src/app/notes/navigation-mobile-notes/navigation-mobile-notes.component.ts
+++ Frontend/src/app/notes/navigation-mobile-notes/navigation-mobile-notes.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { map, Observable, shareReplay } from 'rxjs';
+import { FIXED_SIZES } from 'src/app/config/strings.enum';
 
 @Component({
   selector: 'app-navigation-mobile-notes',
@@ -18,13 +19,13 @@ export class NavigationMobileNotesComponent implements OnInit {
   constructor(private breakpointObserver: BreakpointObserver,) { }
 
   ngOnInit(): void {
-    this.deviceHeight = window.innerHeight - 60;
+    this.deviceHeight = window.innerHeight - FIXED_SIZES.MOBILE_MENU_HEIGHT_FROM_BOTTOM;
   }
 
   // update value when resize
   @HostListener('window:resize', ['$event'])
   onResize() {
-    this.deviceHeight = window.innerHeight - 60;
+    this.deviceHeight = window.innerHeight - FIXED_SIZES.MOBILE_MENU_HEIGHT_FROM_BOTTOM;
   }
 
 }
