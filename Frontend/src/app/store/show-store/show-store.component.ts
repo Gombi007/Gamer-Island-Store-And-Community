a9fb2034904fa26e05a929df59b865efb585dc4c
Gyula Gombos <gombos88@gmail.com>
2022-12-12 14:35:09 +0100

    get all games method was added

--- Frontend/src/app/store/show-store/show-store.component.ts
+++ Frontend/src/app/store/show-store/show-store.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/config/global.service';
+import { PagInfo } from 'src/app/config/pag-info.model';
+import { storeGame } from '../config/store-game.model';
+import { StoreService } from '../config/store.service';
 
 @Component({
   selector: 'app-show-store',
@@ -7,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowStoreComponent implements OnInit {
   isPending = false;
+  defaultGames: storeGame[] = [];
+  pagInfo: PagInfo = new PagInfo(0, 0, 0);
 
-  constructor() { }
+  constructor(private storeService: StoreService, private globalService: GlobalService) { }
 
   ngOnInit(): void {
+    this.getAllDefaultGames();
+  }
+
+  getAllDefaultGames() {
+    this.isPending = true;
+    this.storeService.getAllDefaultGames(0).subscribe({
+      next: (data) => {
+        this.defaultGames = data.page;
+        this.pagInfo = data.paginationInfo;
+        this.isPending = false;
+      },
+      error: (response) => {
+        this.globalService.isExpiredToken(response);
+        console.log(response);
+        this.isPending = false;
+      }
+    });
   }
 
 }
