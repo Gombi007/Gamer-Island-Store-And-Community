25a5937ad84b289e2cd033f7275491cae033b97b
Gyula Gombos <gombos88@gmail.com>
2022-12-15 11:50:32 +0100

    basic filter was done

--- Frontend/src/app/store/filter-store/filter-store.component.ts
+++ Frontend/src/app/store/filter-store/filter-store.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { storeFilter } from '../config/store-filter.model';
 
 @Component({
@@ -11,6 +11,11 @@ export class FilterStoreComponent implements OnInit {
   isPending = false;
   filterForm: FormGroup
   filterDto: storeFilter = new storeFilter();
+  opSystems: string[] = ['Windows', 'Mac', 'Linux'];
+  languages: string[] = ['English', 'Hungarian', 'German'];
+  genres: string[] = ['Action', 'Indie', 'RPG'];
+  categories: string[] = ['Steam Cloud', 'Full controller support', 'MMO'];
+  selectedPrice = 70;
 
   constructor() { }
 
@@ -22,6 +27,14 @@ export class FilterStoreComponent implements OnInit {
     return this.filterForm = new FormGroup({
       'sortByField': new FormControl('name'),
       'isAscending': new FormControl('true'),
+      'languages': new FormControl(['English']),
+      'genres': new FormControl([]),
+      'opSystems': new FormControl([]),
+      'categories': new FormControl([]),
+      'isHideFreeGames': new FormControl(false),
+      'isHideMyOwnGames': new FormControl(false),
+      'isHideMyWishlistGames': new FormControl(false),
+      'price': new FormControl(70),
     });
   }
 
@@ -33,6 +46,17 @@ export class FilterStoreComponent implements OnInit {
   //convenience getter for easy access to form fields
   get formControl(): { [key: string]: AbstractControl; } {
     return this.filterForm.controls;
+  }
+
+  get priceWithText(): string {
+    let resultText = '';
+    let actualPrice = this.formControl['price'].value;
+    actualPrice === 0 ? resultText = 'Free' : '';
+    actualPrice === 75 ? resultText = 'Any Price' : '';
+    actualPrice > 0 && actualPrice < 75 ? resultText = 'Under ' + actualPrice + ' €' : '';
+
+
+    return resultText;
   }
 
 
