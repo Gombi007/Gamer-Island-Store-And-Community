156546557160b72a0789b917a936653bcacf4072
Gyula Gombos <gombos88@gmail.com>
2022-12-15 14:44:25 +0100

    filter method was done

--- Frontend/src/app/store/filter-store/filter-store.component.ts
+++ Frontend/src/app/store/filter-store/filter-store.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
+import { filter, Subscription } from 'rxjs';
 import { storeFilter } from '../config/store-filter.model';
+import { StoreService } from '../config/store.service';
 
 @Component({
   selector: 'app-filter-store',
@@ -10,22 +12,27 @@ import { storeFilter } from '../config/store-filter.model';
 export class FilterStoreComponent implements OnInit {
   isPending = false;
   filterForm: FormGroup
-  filterDto: storeFilter = new storeFilter();
   opSystems: string[] = ['Windows', 'Mac', 'Linux'];
   languages: string[] = ['English', 'Hungarian', 'German'];
   genres: string[] = ['Action', 'Indie', 'RPG'];
   categories: string[] = ['Steam Cloud', 'Full controller support', 'MMO'];
   isShowFullFilter = false;
-  constructor() { }
+  formValueChangeSub: Subscription;
+
+  constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
     this.createFilterForm();
+    this.formValueChangeSub = this.filterForm.valueChanges.subscribe((data) => {
+      let filter: storeFilter = new storeFilter(data);
+      this.storeService.storeFilter.next(filter);
+    });
   }
 
   createFilterForm() {
     return this.filterForm = new FormGroup({
       'sortByField': new FormControl('name'),
-      'isAscending': new FormControl('true'),
+      'isAscending': new FormControl(true),
       'languages': new FormControl(['English']),
       'genres': new FormControl([]),
       'opSystems': new FormControl([]),
@@ -34,12 +41,10 @@ export class FilterStoreComponent implements OnInit {
       'isHideMyOwnGames': new FormControl(false),
       'isHideMyWishlistGames': new FormControl(false),
       'price': new FormControl(75),
+      'showOnlyAdultGames': new FormControl(false),
+      'isHideAdultGames': new FormControl(true)
+
     });
-  }
-
-  collectFormData() {
-    console.log(this.filterForm.value);
-
   }
 
   //convenience getter for easy access to form fields
@@ -54,6 +59,10 @@ export class FilterStoreComponent implements OnInit {
     actualPrice === 75 ? resultText = 'Any Price' : '';
     actualPrice > 0 && actualPrice < 75 ? resultText = 'Under ' + actualPrice + ' €' : '';
     return resultText;
+  }
+
+  get allFilterResult(): number {
+    return this.storeService.allFilterResult;
   }
 
   changeCloseOrOpenFilterPanel() {
