5e366fbe0f60ee53d694ca7c69eff872b3068b9d
Gyula Gombos <gombos88@gmail.com>
2022-12-15 14:42:23 +0100

    store service was extended with filter subject

--- Frontend/src/app/store/config/store.service.ts
+++ Frontend/src/app/store/config/store.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { AuthorizationService } from "src/app/config/authorization.service";
 import { PagInfo } from "src/app/config/pag-info.model";
 import { STRINGS } from "src/app/config/strings.enum";
@@ -10,14 +11,14 @@ import { storeGame } from "./store-game.model";
     providedIn: 'root'
 })
 export class StoreService {
-
-    filter: storeFilter = new storeFilter();
+    storeFilter: Subject<storeFilter> = new Subject<storeFilter>();
+    defaultStoreFilter = new storeFilter();
     defaultSize: number = 18;
+    allFilterResult = 0;
 
     constructor(private http: HttpClient, private authorServie: AuthorizationService) { }
 
-    getAllDefaultGames(page: number) {
-        return this.http.post<{ 'page': storeGame[], 'paginationInfo': PagInfo }>(STRINGS.SERVER_URL + STRINGS.API_GAMES + '?page=' + page + '&size=' + this.defaultSize, this.filter, this.authorServie.headerWithTokenForRequests());
-
+    getAllDefaultGames(page: number, storeFilter: storeFilter = this.defaultStoreFilter) {
+        return this.http.post<{ 'page': storeGame[], 'paginationInfo': PagInfo }>(STRINGS.SERVER_URL + STRINGS.API_GAMES + '?page=' + page + '&size=' + this.defaultSize, storeFilter, this.authorServie.headerWithTokenForRequests());
     }
 }
