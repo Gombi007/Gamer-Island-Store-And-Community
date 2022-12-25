323c9455c2d74c90334d240b8a9a47f51dd1225c
Gombi007 <gombos88@gmail.com>
2022-12-25 14:02:43 +0100

    main admin page was modified with new moduls

--- Frontend/src/app/admin/admin.component.ts
+++ Frontend/src/app/admin/admin.component.ts
@@ -8,65 +8,7 @@ import { AdminService } from './config/admin.service';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnDestroy {
-  isSavingInProgress: boolean = false;
-  savedGames: string[] = [];
-  repeatGameSaveQuerryTime = interval(360000);
-  subSaveGamesInterval: Subscription;
-  subsSaveGame: Subscription[] = [];
-  actualSavingCircle: number = 0;
-  maximumSavingCircle: number = 20;
+export class AdminComponent {
 
-  constructor(private adminService: AdminService, private globalService: GlobalService) { }
-
-  doSaving() {
-    if (this.subSaveGamesInterval === undefined || this.subSaveGamesInterval.closed) {
-      this.isSavingInProgress = true;
-      this.actualSavingCircle++;
-      this.saveGamesFromSteamToDb();
-      this.subSaveGamesInterval = this.repeatGameSaveQuerryTime
-        .pipe(take(this.maximumSavingCircle - 1))
-        .subscribe(() => {
-          this.actualSavingCircle++;
-          this.saveGamesFromSteamToDb();
-        });
-
-    } else {
-      this.subSaveGamesInterval.unsubscribe();
-      if (this.subsSaveGame.length > 0) {
-        this.subsSaveGame.forEach(sub => { sub.unsubscribe() })
-        this.subsSaveGame = [];
-      }
-      this.isSavingInProgress = false;
-      this.actualSavingCircle = 0;
-    }
-  }
-
-  saveGamesFromSteamToDb() {
-    this.subsSaveGame.push(this.adminService.saveNewGamesFromSteamToDb().subscribe({
-      next: (data: any) => {
-        this.savedGames = data;
-        if (this.actualSavingCircle === this.maximumSavingCircle) {
-          this.isSavingInProgress = false;
-          this.actualSavingCircle = 0
-        }
-      },
-      error: (response) => {
-        this.isSavingInProgress = false;
-        this.actualSavingCircle = 0
-        this.globalService.isExpiredToken(response);
-      }
-    }));
-  }
-
-  ngOnDestroy(): void {
-    if (this.subSaveGamesInterval !== undefined) {
-      this.subSaveGamesInterval.unsubscribe();
-    }
-    if (this.subsSaveGame.length > 0) {
-      this.subsSaveGame.forEach(sub => { sub.unsubscribe() })
-      this.subsSaveGame = [];
-    }
-  }
 
 }
